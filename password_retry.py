c32f5bb8603c3e25819a71a5b9508c8d5c266c30
Fu Chen Yuan <shes830501@gmail.com>
2020-09-25 16:12:10 +0800

    modified

--- password_retry.py
+++ password_retry.py
@@ -5,10 +5,14 @@
 password = 'a123456'
 x = 3 #剩餘機會
 while x > 0:
+	x =  x - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功!') 
 		break #逃出迴圈
 	else: 
-		x =  x - 1
-		print('密碼錯誤! 還有' , x, '次機會')
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有' , x, '次機會')
+		else:
+			print('沒機會嘗試了!要鎖帳號了啦!')
